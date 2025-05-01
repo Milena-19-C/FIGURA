fe53de7a0c628ca6ffdaa1e5a520d024f2444880
User <ug-fca@ug.edu.gob>
2025-05-01 11:56:49 -0500

    Revert "FIGURAS GEOMETRICAS"
    This reverts commit a11a61a25135637b11dea36785fbcf98cc917dc9.

---
+++ Cuadrado.py
@@ -0,0 +1,10 @@
+
+class Cuadrado(figurageometrica):
+    def _init_(self, lado):
+        super()._init_(lado, lado)
+
+    def area(self):
+        return self.alto * self.ancho
+
+    def _str_(self):
+        return f"Cuadrado - lado: {self.alto}, área: {self.area()}"
