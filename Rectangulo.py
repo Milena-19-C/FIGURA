8138536ce4807112d3a57ecb5a5029d99251e043
User <ug-fca@ug.edu.gob>
2025-05-07 08:53:01 -0500

    Revert "FIGURAS GEOMETRICAS"
    This reverts commit a11a61a25135637b11dea36785fbcf98cc917dc9.

---
+++ Rectangulo.py
@@ -0,0 +1,10 @@
+class Rectangulo(figurageometrica):
+    def _init_(self, alto, ancho):
+        super()._init_(alto, ancho)
+
+    def area(self):
+        return self.alto * self.ancho
+
+    def _str_(self):
+        return f"Rectángulo - alto: {self.alto}, ancho: {self.ancho}, área: {self.area()}"
+
---
+++ figurageometrica.py
@@ -0,0 +1,19 @@
+class figurageometrica:
+    def _init_(self, alto, ancho):
+        self.alto = alto
+        self.ancho = ancho
+@property
+def alto(self):
+    return self._alto
+@alto.setter
+def alto(self, valor):
+    self._alto = valor
+@property
+def ancho(self):
+    return self._ancho
+@ancho.setter
+def ancho(self, valor):
+    self._ancho = valor
+
+def _int_(self):
+    return self.alto,
